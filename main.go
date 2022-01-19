98053d4a0b5eca38e8c82b2280b6feb306cfe174
Ahmed Samir <a.samir9710@gmail.com>
2022-01-19 02:41:12 +0200

    Started Chat application using Golang

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+var addr = flag.String("addr", ":8080", "http server address")
+
+func main() {
+	flag.Parse()
+	wsServer := NewWebSocketServer()
+	go wsServer.Run()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		ServeWs(wsServer, w, r)
+	})
+
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
